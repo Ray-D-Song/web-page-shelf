ba03a9ce50a34ad370b2b2c87b4f09c03bed5fa4
Ray <songraysmail@gmail.com>
2024-06-25 15:34:32 +0800

    add: unocss and login page

--- packages/server/src/renderer.tsx
+++ packages/server/src/renderer.tsx
@@ -4,10 +4,16 @@ export const renderer = jsxRenderer(({ children, title }) => {
   return (
     <html>
       <head>
-        <link href="/static/style.css" rel="stylesheet" />
+        {
+          import.meta.env.PROD
+            ? (<link href="/static/style.css" rel="stylesheet" />)
+            : (<link href="/src/style.css" rel="stylesheet" />)
+        }
+        <link href="/static/reset.css" rel="stylesheet" />
+        <link href="/static/util.css" rel="stylesheet" />
         <title>{title}</title>
       </head>
-      <body>{children}</body>
+      <body className="bg-gray-100 grid-background">{children}</body>
     </html>
   )
 })
--- packages/server/vite.config.ts
+++ packages/server/vite.config.ts
@@ -2,13 +2,21 @@ import build from '@hono/vite-cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
+import UnoCSS from '@unocss/postcss'
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        UnoCSS(),
+      ]
+    }
+  },
   plugins: [
     build(),
     devServer({
       adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
+      entry: 'src/index.tsx',
+    }),
+  ],
 })
