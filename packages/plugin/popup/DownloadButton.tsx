0fb6159bd8b17fb87f6417c24e4c95768deb2f1e
banzhe <banzhe5@outlook.com>
2024-06-28 07:58:17 +0800

    feat: integrate singlefile

--- packages/plugin/popup/DownloadButton.tsx
+++ packages/plugin/popup/DownloadButton.tsx
@@ -1,11 +1,26 @@
 function DownloadButton() {
-  const handleDownload = () => {
+  const handleDownload = async () => {
+    console.log("Download button clicked");
     // Add your download logic here
+    const { content, title, filename } = await extension.getPageData({
+      removeHiddenElements: true,
+      removeUnusedStyles: true,
+      removeUnusedFonts: true,
+      removeImports: true,
+      blockScripts: true,
+      blockAudios: true,
+      blockVideos: true,
+      compressHTML: true,
+      removeAlternativeFonts: true,
+      removeAlternativeMedias: true,
+      removeAlternativeImages: true,
+      groupDuplicateImages: true
+    });
+    console.log(content, title, filename);
   }
 
   return (
-    <div>
-      <h2 className="color-red">Download</h2>
+    <div >
       <button onClick={handleDownload}>
         Download
       </button>
--- packages/plugin/popup/main.tsx
+++ packages/plugin/popup/main.tsx
@@ -1,4 +1,5 @@
 import '../styles'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import DownloadButton from './DownloadButton'
 
@@ -6,8 +7,9 @@ const root = document.getElementById('root')
 if (!root) {
   throw new Error('Root element not found')
 }
-const div = document.createElement('div')
-div.id = 'root'
+
 ReactDOM.createRoot(root).render(
-  <DownloadButton></DownloadButton>,
+  <React.StrictMode>
+    <DownloadButton></DownloadButton>
+  </React.StrictMode>
 )
