6ba50b68f2dd85029f70f35e3d2c1288d42adc9e
banzhe <banzhe5@outlook.com>
2024-06-29 15:03:29 +0800

    fix: singlefile script asynchronous  loading issue

---
+++ packages/plugin/lib/content-script-all-frames-lib.js
@@ -0,0 +1,3 @@
+import './chrome-browser-polyfill.js'
+import './single-file-frames.js'
+import './single-file-extension-frames.js'
---
+++ packages/plugin/lib/content-script-lib.js
@@ -0,0 +1,4 @@
+import './chrome-browser-polyfill.js'
+import './single-file-bootstrap.js'
+import './single-file-extension-core.js'
+import './single-file.js'
