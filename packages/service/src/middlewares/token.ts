1e179520e3d86818f8e2b9d4c08fbb0d37661531
Ray-D-Song <songraysmail@gmail.com>
2024-07-15 19:13:45 +0800

    eslint fix

--- packages/service/src/middlewares/token.ts
+++ packages/service/src/middlewares/token.ts
@@ -5,14 +5,14 @@ import result from '@/utils/result'
 
 export default createMiddleware(async (c, next) => {
   const token = getCookie(c, 'token')
-  if(!token) {
+  if (!token) {
     return c.json(result.error(401, 'Unauthorized'))
   }
   const JWT_SECRET = import.meta.env.MODE === 'development' ? 'dev' : c.env.JWT_SECRET
   const decodedPayload = await verify(token, JWT_SECRET)
   const userId = decodedPayload.id
   const tk = c.env.KV.get(`token:${userId}`)
-  if(!tk || tk !== token) {
+  if (!tk || tk !== token) {
     deleteCookie(c, 'token')
     return c.json(result.error(401, 'Unauthorized'))
   }
